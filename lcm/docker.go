7507368ed0150b2dd165e6677280bdf39a938e3f
Jacob Poye <jacob@skyramp.dev>
2024-10-15 14:16:01 -0700

    Go Library changes - v0.4.99 (#19)
    Co-authored-by: texmachina <texmachina@users.noreply.github.com>

--- lcm/docker.go
+++ lcm/docker.go
@@ -13,7 +13,8 @@ import (
 	"time"
 
 	dockerTypes "github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
+	dockerContainer "github.com/docker/docker/api/types/container"
+	dockerImage "github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/api/types/volume"
 	docker "github.com/docker/docker/client"
@@ -36,7 +37,7 @@ type DockerLCM struct {
 }
 
 var (
-	DockerEngineAPIVersion = "1.41"
+	DockerEngineAPIVersion = "1.44"
 )
 
 // generic docker LCM to communicate with docker API
@@ -84,7 +85,7 @@ func (d *DockerLCM) RestartWithNewPorts(container *dockerTypes.ContainerJSON, po
 
 	// stop and remove existing container
 	logger.Infof("attempting to kill container %s", name)
-	if err := d.client.ContainerRemove(ctx, container.ID, dockerTypes.ContainerRemoveOptions{Force: true}); err != nil {
+	if err := d.client.ContainerRemove(ctx, container.ID, dockerContainer.RemoveOptions{Force: true}); err != nil {
 		logger.Errorf("failed to remove container %s: %v", name, err)
 		return fmt.Errorf("failed to remove container %s: %v", name, err)
 	}
@@ -100,7 +101,7 @@ func (d *DockerLCM) RestartWithNewPorts(container *dockerTypes.ContainerJSON, po
 		return fmt.Errorf("failed to create a container %s: %w", name, err)
 	}
 
-	if err := d.client.ContainerStart(ctx, resp.ID, dockerTypes.ContainerStartOptions{}); err != nil {
+	if err := d.client.ContainerStart(ctx, resp.ID, dockerContainer.StartOptions{}); err != nil {
 		logger.Errorf("failed to start container %s: %v", name, err)
 		return fmt.Errorf("failed to start container %s: %v", name, err)
 	}
@@ -120,7 +121,7 @@ func RunSkyrampWorkerInDockerNetwork(image, tag string, hostPort int, targetNetw
 	}
 
 	// Create a Docker volume
-	vol, err := dockerLcm.client.VolumeCreate(context.Background(), volume.VolumeCreateBody{
+	vol, err := dockerLcm.client.VolumeCreate(context.Background(), volume.CreateOptions{
 		Name: containerName,
 	})
 
@@ -130,7 +131,7 @@ func RunSkyrampWorkerInDockerNetwork(image, tag string, hostPort int, targetNetw
 	imagePath := fmt.Sprintf("%s:%s", image, tag)
 	_, _, err = dockerLcm.client.ImageInspectWithRaw(context.Background(), imagePath)
 	if err != nil {
-		out, err := dockerLcm.client.ImagePull(context.Background(), imagePath, dockerTypes.ImagePullOptions{})
+		out, err := dockerLcm.client.ImagePull(context.Background(), imagePath, dockerImage.PullOptions{})
 		if err != nil {
 			return fmt.Errorf("error occurred while pulling image %s: %v", imagePath, err)
 		}
@@ -142,7 +143,7 @@ func RunSkyrampWorkerInDockerNetwork(image, tag string, hostPort int, targetNetw
 	}
 	workerContainer, err := dockerLcm.client.ContainerCreate(
 		context.Background(),
-		&container.Config{
+		&dockerContainer.Config{
 			Image: imagePath,
 			Volumes: map[string]struct{}{
 				types.DefaultWorkerConfigDirPath: {},
@@ -151,11 +152,11 @@ func RunSkyrampWorkerInDockerNetwork(image, tag string, hostPort int, targetNetw
 				nat.Port(containerExposedPort): {},
 			},
 		},
-		&container.HostConfig{
+		&dockerContainer.HostConfig{
 			Binds: []string{
 				fmt.Sprintf("%s:%s:rw", vol.Name, types.DefaultWorkerConfigDirPath),
 			},
-			RestartPolicy: container.RestartPolicy{
+			RestartPolicy: dockerContainer.RestartPolicy{
 				Name: "always",
 			},
 			PortBindings: map[nat.Port][]nat.PortBinding{
@@ -169,7 +170,7 @@ func RunSkyrampWorkerInDockerNetwork(image, tag string, hostPort int, targetNetw
 		return fmt.Errorf("error occurred while creating container %s: %v", containerName, err)
 	}
 
-	err = dockerLcm.client.ContainerStart(context.Background(), workerContainer.ID, dockerTypes.ContainerStartOptions{})
+	err = dockerLcm.client.ContainerStart(context.Background(), workerContainer.ID, dockerContainer.StartOptions{})
 	if err != nil {
 		return fmt.Errorf("error occurred while starting container %s: %v", containerName, err)
 	}
@@ -192,7 +193,7 @@ func RunSkyrampWorkerInDockerNetwork(image, tag string, hostPort int, targetNetw
 			break
 		}
 
-		err = dockerLcm.client.ContainerStop(context.Background(), dockerLcm.Container.ID, nil)
+		err = dockerLcm.client.ContainerStop(context.Background(), dockerLcm.Container.ID, dockerContainer.StopOptions{})
 		if err != nil {
 			return fmt.Errorf("error occurred while stoping container %s: %v", containerName, err)
 		}
@@ -204,7 +205,7 @@ func RunSkyrampWorkerInDockerNetwork(image, tag string, hostPort int, targetNetw
 		if err != nil {
 			return fmt.Errorf("error occurred while attaching container %s to %s: %v", containerName, targetNetwork.ID, err)
 		}
-		err = dockerLcm.client.ContainerStart(context.Background(), dockerLcm.Container.ID, dockerTypes.ContainerStartOptions{})
+		err = dockerLcm.client.ContainerStart(context.Background(), dockerLcm.Container.ID, dockerContainer.StartOptions{})
 		if err != nil {
 			return fmt.Errorf("error occurred while starting container %s: %v", containerName, err)
 		}
@@ -259,7 +260,7 @@ func RemoveSkyrampWorkerInDocker(containerName string) error {
 	}
 
 	// delete container
-	_ = dockerLcm.client.ContainerRemove(context.Background(), workerContainer.ID, dockerTypes.ContainerRemoveOptions{Force: true})
+	_ = dockerLcm.client.ContainerRemove(context.Background(), workerContainer.ID, dockerContainer.RemoveOptions{Force: true})
 
 	// delete docker volume
 	err = dockerLcm.client.VolumeRemove(context.Background(), containerName, true)
@@ -276,7 +277,7 @@ func findContainerByName(containerName string) (*dockerTypes.Container, error) {
 		return nil, err
 	}
 
-	containers, err := dockerLcm.client.ContainerList(context.Background(), dockerTypes.ContainerListOptions{All: true})
+	containers, err := dockerLcm.client.ContainerList(context.Background(), dockerContainer.ListOptions{All: true})
 	if err != nil {
 		return nil, err
 	}
@@ -318,7 +319,7 @@ func (d *DockerLCM) FindNetworkByName(networkName string) (*dockerTypes.NetworkR
 // find container in docker using address and IP
 // there could be some edges cases that this does not properly handle
 func (d *DockerLCM) FindContainerByBoundAddress(address string) (*dockerTypes.ContainerJSON, error) {
-	containers, err := d.client.ContainerList(context.Background(), dockerTypes.ContainerListOptions{})
+	containers, err := d.client.ContainerList(context.Background(), dockerContainer.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
